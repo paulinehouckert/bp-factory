32cc24a6498d515c47c6ed4a88306d552b33e2e2
paulinehouckert <86850069+paulinehouckert@users.noreply.github.com>
2022-03-28 12:33:12 +0200

    Theme setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./layout/*.liquid",
+    "./templates/*.liquid",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
