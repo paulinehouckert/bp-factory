06d4e4e9c3ccbaef46c3e0b5758f48399312b9eb
paulinehouckert <86850069+paulinehouckert@users.noreply.github.com>
2022-03-28 16:24:28 +0200

    Theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
     "./templates/*.liquid",
   ],
   theme: {
+    colors: {
+      white: "#ffffff",
+      light: "rgba(255, 255, 255, 0.7)",
+      dark: "#3B3B3B",
+      "gray-1": "#3C3C3C",
+      "gray-2": "#898989",
+      "gray-3": "#6E6E6D",
+      "neutral-1": "#E5E5E3",
+      "neutral-2": "#E5E5E5",
+    },
+    fontFamily: {
+      theme: ["Montserrat", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
