b54319c9232ae63c344789e90cdc23eaad07be37
paulinehouckert <86850069+paulinehouckert@users.noreply.github.com>
2022-04-03 23:35:26 +0200

    NL terms

--- assets/theme.js
+++ assets/theme.js
@@ -29,15 +29,30 @@ window.addEventListener("load", () => {
 
 const $checkbox = document.querySelector("#checkbox");
 const $checkbox_checked = document.querySelector("#checkbox-checked");
+const $checkbox_input = document.querySelector("#terms");
 
 if ($checkbox) {
   $checkbox.addEventListener("click", () => {
     $checkbox.classList.remove("active");
     $checkbox_checked.classList.add("active");
+    $checkbox_input.checked = true;
   });
 
   $checkbox_checked.addEventListener("click", () => {
     $checkbox.classList.add("active");
     $checkbox_checked.classList.remove("active");
+  });
+}
+
+// Check if the checkbox is checked
+
+const $newsletter_form = document.querySelector("#newsletter-form");
+
+if ($newsletter_form) {
+  $newsletter_form.addEventListener("submit", (e) => {
+    if (!$checkbox_input.checked) {
+      e.preventDefault();
+      alert("Please accept the terms and conditions");
+    }
   });
 }
